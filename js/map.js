afb95c7878c19cc43f6a011fb1f49eab3491bf1e
Giuseppe Naponiello <beppenapo@arc-team.com>
2016-03-10 12:07:20 +0100

    1603101200 fixed newpoi

--- js/map.js
+++ js/map.js
@@ -5,28 +5,28 @@ var map, extent, gsat, osm, arrayOSM, arrayAerial, baseOSM, baseAerial, poi, hig
 var info, geolocate, filter, layers,selectFeatureControl, stile;//controlli
 
 $(document).ready(function() {
- 
- if(windowX < 481){ //smartphone         
+
+ if(windowX < 481){ //smartphone
   switcherWidth = "150px";
   resultWidth = "99%";
   $(".switcherIco").remove();
  }
  else if(windowX < 1024){ //tablet
   switcherWidth = "180px";
-  resultWidth = "250px"; 
+  resultWidth = "250px";
  }
  else { //pc
   switcherWidth = "200px";
   resultWidth = "350px";
  }
- 
+
  $("#result").css("width", resultWidth);
 
- $("#mappaLink").addClass('active').click(function(e){e.preventDefault();}); 
+ $("#mappaLink").addClass('active').click(function(e){e.preventDefault();});
 
  $("#map").height(windowY-headH-5);
  $("#menuMap").css("width", "22px");
- 
+
  $("#switcher, #result").hide();
  $("#menuMapIco").clickToggle(
   function(){
@@ -43,9 +43,9 @@ $(document).ready(function() {
 
  $(".baselayers").on('change', function(){
   $(".baselayers").closest('label').removeClass('layerActive');
-  $(this).closest('label').addClass('layerActive'); 
+  $(this).closest('label').addClass('layerActive');
  });
- 
+
  $(".overlayers").on('change', function(){
    if($(this).is(':checked')){$(this).prev('img').fadeTo('slow', 1);}else{$(this).prev('img').fadeTo('slow', 0.3);}
    $(this).closest('label').toggleClass('layerActive');
@@ -58,24 +58,24 @@ $(document).ready(function() {
      filter='';
      filter = 'id_icone>0';
      info.cql_filter=filter;
-     poi.params.CQL_FILTER = filter; 
+     poi.params.CQL_FILTER = filter;
      poi.setVisibility(true);
      //poi.redraw();
      return;
    }
-   else{ 
+   else{
     $('input[name="tipologia"]:checked').each(function(){
       var param = $(this).val();
       filter += 'id_icone='+param+' OR ';
     });
     filter = filter.slice(0,-4);
     //console.log(filter);
-    poi.params.CQL_FILTER = filter;  
-    info.cql_filter=filter;  
+    poi.params.CQL_FILTER = filter;
+    info.cql_filter=filter;
    }
    poi.setVisibility(true);
    poi.redraw();
- }); 
+ });
 });
 
 
@@ -96,12 +96,12 @@ function init() {
    displayProjection: new OpenLayers.Projection("EPSG:4326")
  });
 
- 
+
 arrayOSM = ["http://otile1.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.jpg",
             "http://otile2.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.jpg",
             "http://otile3.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.jpg",
             "http://otile4.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.jpg"];
-            
+
 baseOSM = new OpenLayers.Layer.OSM("MapQuest-OSM Tiles", arrayOSM, {transitionEffect: "resize"});
 map.addLayer(baseOSM);
 
@@ -143,8 +143,8 @@ layers=[poi];
 console.log('cql_filter: '+filter);
 
 info = new OpenLayers.Control.WMSGetFeatureInfo({
-    url: 'http://37.187.200.160:8080/geoserver/rete/wms', 
-    title: 'Informazioni sui livelli interrogati',  
+    url: 'http://37.187.200.160:8080/geoserver/rete/wms',
+    title: 'Informazioni sui livelli interrogati',
     queryVisible: true,
     //cql_filter: filter,
     layers: layers,
@@ -153,11 +153,12 @@ info = new OpenLayers.Control.WMSGetFeatureInfo({
     eventListeners: {
         getfeatureinfo: function(event) {
             var arr = new Array();
-            for (var i = 0; i < event.features.length; i++) { 
-               var feature = event.features[i]; 
-               var attributes = feature.attributes; 
-               var id_poi = attributes.id_poi;  
-               arr.push(id_poi);                      
+            for (var i = 0; i < event.features.length; i++) {
+               var feature = event.features[i];
+               var attributes = feature.attributes;
+               var id_poi = attributes.id_poi;
+               console.log(id_poi);
+               arr.push(id_poi);
             }
             $.ajax({
                  url: 'inc/popupPoi.php',
@@ -165,15 +166,15 @@ info = new OpenLayers.Control.WMSGetFeatureInfo({
                  data: {arr:arr},
                  success: function(data){
                    $("#result").fadeIn('fast');
-                   $("#resultContent").html(data);                   
+                   $("#resultContent").html(data);
                    var resDivH = $("#result").outerHeight(true);
                    var resHeaderH = $("#resultHeader").outerHeight(true);
                    $("#resultContent").height(resDivH - resHeaderH -20);
                    $('.trigger').on({
-                     click: function(){        
-                      var ll = $(this).attr('ll'); 
-                      if (ll == 0) return;    
-                      var parse = ll.split(','); 
+                     click: function(){
+                      var ll = $(this).attr('ll');
+                      if (ll == 0) return;
+                      var parse = ll.split(',');
                       var newll= new OpenLayers.LonLat(parse[0],parse[1]);
                       map.setCenter(newll,18);
                      },
@@ -184,11 +185,11 @@ info = new OpenLayers.Control.WMSGetFeatureInfo({
                          selectFeatureControl.select(highlightLayer.features[f]);
                        }
                       }
-                     }, 
-                     mouseleave: function() { 
+                     },
+                     mouseleave: function() {
                       var id_poi=$(this).data('id');
                       featHiLite = highlightLayer.getFeaturesByAttribute('id_poi', id_poi);
-                      highlightLayer.drawFeature(featHiLite[0], "default");  
+                      highlightLayer.drawFeature(featHiLite[0], "default");
                      }
                    });
                  }
@@ -205,8 +206,8 @@ map.addControl(info);
 info.activate();
 
 if (!map.getCenter()) {map.zoomToExtent(extent);}
- 
-$('.olControlZoom').append('<a href="#" id="max" title="torna allo zoom iniziale"><i class="fa fa-globe"></i></a>'); 
+
+$('.olControlZoom').append('<a href="#" id="max" title="torna allo zoom iniziale"><i class="fa fa-globe"></i></a>');
 $('.olControlZoomIn').attr("title","Ingrandisci la mappa");
 $('.olControlZoomOut').attr("title","Diminuisci la mappa");
 $("#max").click(function(){map.zoomToExtent(extent);});
