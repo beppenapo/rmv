a223c63dd12827bc34e65b2003b896010d1e353d
Giuseppe Naponiello <beppenapo@arc-team.com>
2016-03-10 11:49:34 +0100

    1603101150 fixed eliminazione record + file

--- inc/delFoto.php
+++ inc/delFoto.php
@@ -7,15 +7,10 @@ if (is_file($file)){
     if(unlink($file)){
         $del="delete from foto_poi where id_foto = ".$_POST['foto'];
         $exec = pg_query($connection, $del);
-        if(!$exec){
-            die("Errore nella query: \n" . pg_last_error($connection));
-        }else{
-            echo "La foto è stata definitivamente eliminata"; 
-        }
+        if(!$exec){ die("Errore nella query: \n" . pg_last_error($connection)); }else{ echo "La foto è stata definitivamente eliminata"; }
     }else{
         echo "Non è stato possibile eliminare il file.";
     }
-    
 }else{
     echo "Problemi nell'eliminazione del file";
 }
