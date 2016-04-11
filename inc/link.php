49430c6cb634089013d708bfbf45fb2fbba86bee
montopoli server <beppenapo@arc-team.com>
2016-04-11 17:34:59 +0200

    1604111725 aggiunta link comune smm

--- inc/link.php
+++ inc/link.php
@@ -1,7 +1,8 @@
    <ul>
-    <li><a href="http://www.regione.toscana.it/">Regione Toscana</a></li>
-    <li><a href="http://www.provincia.pisa.it/">Provincia di Pisa</a></li>
-    <li><a href="http://www.comune.san-miniato.pi.it/">Comune di San Miniato</a></li>
-    <li><a href="http://www.comune.montopoli.pi.it/">Comune di Montopoli</a></li>
-    <li><a href="http://www.castelfrancodisotto.gov.it/comune.html">Comune di Castelfranco di Sotto</a></li>
+    <li><a href="http://www.regione.toscana.it/" target="_blank" title="[link esterno] sito web della Regione Toscana">Regione Toscana</a></li>
+    <li><a href="http://www.provincia.pisa.it/" target="_blank" title="[link esterno] sito web della Provincia di Pisa">Provincia di Pisa</a></li>
+    <li><a href="http://www.comune.san-miniato.pi.it/" target="_blank" title="[link esterno] sito web del Comune di San Miniato">Comune di San Miniato</a></li>
+    <li><a href="http://www.comune.montopoli.pi.it/" target="_blank" title="[link esterno] sito web del Comune di Montopoli">Comune di Montopoli</a></li>
+    <li><a href="http://www.castelfrancodisotto.gov.it/comune.html" target="_blank" title="[link estero] sito web del Comune di Castelfranco di Sotto">Comune di Castelfranco di Sotto</a></li>
+    <li><a href="http://www.comune.santamariaamonte.pi.it/" target="_blank" title="[link esterno] sito web del Comune di Santa Maria a Monte">Comune di Santa Maria a Monte</a></li>
    </ul>
